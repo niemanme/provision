fd17199c897c8b1ac66f2ba2a1133664f211ac63
niemanme <mniemann96@gmail.com>
2017-07-07 14:13:34 -0500

    Added Subnet Helpers
    Created basic subnet helpers in the addSubnetCommands, imported strings and net to help, included some basic measures to ensure functions don't use improper arguments

--- cli/subnet.go
+++ cli/subnet.go
@@ -2,7 +2,8 @@ package cli
 
 import (
 	"fmt"
-
+	"strings"
+	"net"
 	"github.com/digitalrebar/provision/backend"
 	"github.com/digitalrebar/provision/client/subnets"
 	"github.com/digitalrebar/provision/models"
@@ -116,6 +117,220 @@ func addSubnetCommands() (res *cobra.Command) {
 		Short: fmt.Sprintf("Access CLI commands relating to %v", name),
 	}
 	commands := commonOps(singularName, name, &SubnetOps{})
+	
+	commands = append(commands, &cobra.Command{
+		Use:   "range [subnetName] [startIP] [endIP]",
+		Short: fmt.Sprintf("set the range of a subnet"),
+		Long:  `Helper function to set the range of a given subnet.`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 3 {
+				return fmt.Errorf("%v requires 3 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName :=args[0]
+			StartAddr := args[1]
+			EndAddr := args[2]
+
+			sub, e:= Get(subName)
+			if e != nil{
+				return fmt.Errorf("Error getting subnet: %v", e)
+			}
+
+			IPfirst:=ParseIP(StartAddr)
+			if(IPfirst==nil){
+				return fmt.Errorf("Error: %v is not a valid IP address",StartAddr)
+			}
+
+			IPlast:=ParseIP(EndAddr)
+			if(IPlast==nil){
+				return fmt.Errorf("Error: %v is not a valid IP address",EndAddr)
+			}
+
+			sub.ActiveStart = IPfirst
+			sub.ActiveEnd = IPlast
+			return fmt.Printf("startIP: %v\nendIP: %v\n",sub.ActiveStart,sub.ActiveEnd)
+		},
+	})
+
+
+	commands = append(commands, &cobra.Command{
+		Use:   "subnet [subnetName] [subnet CIDR]",
+		Short: fmt.Sprintf("Set the CIDR network address"),
+		Long:  `Helper function to set the CIDR of a given subnet.`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 2 {
+				return fmt.Errorf("%v requires 2 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName:=args[0]
+			CIDR:= args[1]
+
+			sub, e:= Get(subName)
+			if e != nil{
+				return fmt.Errorf("Error getting subnet: %v", e)
+			}
+
+			ip, e2:=ParseCIDR(CIDR)
+			if e2!=nil{
+				return	fmt.Errorf("Error: %v is not a valid subnet CIDR", CIDR)
+
+			}
+			sub.Subnet = CIDR
+
+			return fmt.Printf("%v",sub.Subnet)
+			
+		},
+	})
+
+	commands = append(commands, &cobra.Command{
+		Use:   "strategy [subnetName] [MAC]",
+		Short: fmt.Sprintf("Set Subnet strategy"),
+		Long:  `Helper function to get the range of a given subnet.`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 2 {
+				return fmt.Errorf("%v requires 2 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName:=args[0]
+			MACAddress:=args[1]
+
+			sub, e:= Get(subName)
+			if e != nil{
+				fmt.Errorf("Error getting subnet: %v", e)
+			}
+			sub.Strategy = MACAddress
+			return sub.payload, nil
+		},
+	})
+
+	commands = append(commands, &cobra.Command{
+		Use:   "pickers [subnetName] [list]",
+		Short: fmt.Sprintf("assigns IP allocation methods to a subnet"),
+		Long:  `Helper function that accepts a string of methods to allocate IP addresses separated by commas`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 2 {
+				return fmt.Errorf("%v requires 2 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName:=args[0]
+			pickerString=args[1]
+			sub, e:= Get(subName)
+			if e != nil{
+				fmt.Errorf("Error getting subnet: %v", e)
+			}
+			//ADD PROTECTION
+			sub.Pickers = strings.Split(pickerString)
+			return fmt.Println(sub.Pickers)
+			
+		},
+	})
+
+	commands = append(commands, &cobra.Command{
+		Use:   "nextserver [subnetName] [IP]",
+		Short: fmt.SPrintf("Set next non-reserved IP"),
+		Long:  `Helper function to set the first non-reserved IP of a subnet.`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 2 {
+				return fmt.Errorf("%v requires 2 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName:=args[0]
+			IPAddr:= args[1]
+
+			sub, e:= Get(subName)
+			if e != nil{
+				fmt.Errorf("Error getting subnet: %v", e)
+			}
+
+			nextIP:=ParseIP(IPAddr)
+			if(IPfirst==nil){
+				return fmt.Errorf("Error: %v is not a valid IP address",IPAddr)
+			}
+
+			sub.NextServer=nextIP
+			
+			return sub.payload, nil
+		},
+	})
+
+	commands = append(commands, &cobra.Command{
+		Use:   "leasetimes [subnetName] [active] [reserved]",
+		Short: fmt.Sprintf("Set the leasetimes of a subnet"),
+		Long:  `Helper function to get the range of a given subnet.`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 3 {
+				return fmt.Errorf("%v requires 3 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName:=args[0]
+			activeTime:=args[1]
+			reservedTime:=args[2]
+
+			sub, e:= Get(subName)
+			if e != nil{
+				fmt.Errorf("Error getting subnet: %v", e)
+			}
+			sub.ActiveStart=activeTime
+			sub.ActiveEnd=activeTime
+			return sub.payload, nil
+
+
+
+		},
+	})
+
+commands = append(commands, &cobra.Command{
+		Use:   "set [subnetName] option [number] to [value]",
+		Short: fmt.Sprintf("Set the given subnet's dhcpOption to a value"),
+		Long:  `Helper function that sets the specified dhcpOption from a given subnet to a value. You cannot add an option.`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 5 {
+				return fmt.Errorf("%v requires 5 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName:=args[0]
+			changeval:=args[2] 
+			newval:=args[4]
+
+			sub, e:= Get(subName)
+			if e != nil{
+				fmt.Errorf("Error getting subnet: %v", e)
+			}
+			if changeval>=len(sub.Options){
+				else return fmt.Errorf("Error: option %v does not exist",changeval)
+			}
+
+			sub.Options[changeval]=newval
+			return fmt.Printf("%v\n",subs.Options[changeval])
+		},
+	})
+
+
+	commands = append(commands, &cobra.Command{
+		Use:   "get [subnetName] option [number]",
+		Short: fmt.Sprintf("Get dhcpOption [number]"),
+		Long:  `Helper function that gets the specified dhcpOption from a given subnet.`,
+		RunE: func(c *cobra.Command, args []string) error {
+			if len(args) != 3 {
+				return fmt.Errorf("%v requires 3 arguments", c.UseLine())
+			}
+			dumpUsage=false
+			subName:=args[0]
+			getVal:=args[2]
+			sub, e:= Get(subName)
+			if e != nil{
+				fmt.Errorf("Error getting subnet: %v", e)
+			}
+			if len(sub.Options)<=getVal{
+				else return fmt.Errorf("Error: option %v does not exist",getVal)
+			}
+			
+			return fmt.Println("Option %v: %v",getVal,sub.Options[getVal])
+
+		},
+	})
+
+	
 	res.AddCommand(commands...)
 	return res
 }
